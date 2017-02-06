81b5479466249fd3a2feb62304056c0702c2c769
chamlion <chamlionchina@gmail.com>
2017-02-06 11:36:44 +0000

    Finish user edit, update, index, and destroy actions

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->toArray());
+
+        $user = User::find(1);
+        $user->name = 'chamlion';
+        $user->email = 'chamlionchina@gmail.com';
+        $user->password = '111111';
+        $user->is_admin = true;
+        $user->save();
+    }
+}
