fe84b6f3b483cc1c0420f3fcd48b00d0366856d9
chamlion <chamlionchina@gmail.com>
2017-02-08 14:19:37 +0000

    add a word

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -63,7 +63,7 @@ class UsersController extends Controller
         $from = 'Chamlionchina@gmail.com';
         $name = 'Chamlion';
         $to = $user->email;
-        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+        $subject = "感谢注册Chamlion'Blog！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->from($from, $name)->to($to)->subject($subject);
